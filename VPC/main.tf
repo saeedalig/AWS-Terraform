37432e2dd97853e3d94e789982a1a59491d950f0
Ahmad Saeed <101182798+saeedalig@users.noreply.github.com>
2024-04-05 03:27:40 +0530

    Subnets added

--- VPC/main.tf
+++ VPC/main.tf
@@ -6,3 +6,25 @@ resource "aws_vpc" "main" {
    Name = "Project VPC"
  }
 }
+
+# Create Public Subnets
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+
+# Create Private Subnets
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
